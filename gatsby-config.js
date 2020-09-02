d2c03364124b0a3763fe2a72e57851d96d7b7d6a
David MacEachern <david.maceachern.tech@gmail.com>
2020-09-02 09:49:52 +0200

    Add code

--- gatsby-config.js
+++ gatsby-config.js
@@ -209,10 +209,10 @@ module.exports = {
       options: {
         // Either `code` or `selfHostUrl` is required.
         // REQUIRED IF USING HOSTED GOATCOUNTER! https://[my_code].goatcounter.com
-        code: 'YOUR_GOATCOUNTER_PAGE_CODE',
+        code: 'dmac',
 
         // REQUIRED IF USING SELFHOSTED GOATCOUNTER!
-        selfHostUrl: `https://example.com`,
+        // selfHostUrl: `https://davidmaceachern.com`,
 
         // ALL following settings are OPTIONAL
 
